3ba47f6817b4abdd3fd78ad555931ca049050640
Udiel-Oliveira <udiel.inaciosilva@gmail.com>
2025-05-27 10:36:10 -0300

    Feat: melhorando o SEO e adicionando o icone da pagina

--- ambar-home/src/app/page.tsx
+++ ambar-home/src/app/page.tsx
@@ -5,6 +5,32 @@ import { PitchSection } from "@/components/HomePage/pitchSection";
 import { ScrollContainer } from "@/components/HomePage/scrollContainer";
 import { SupportSection } from "@/components/HomePage/supportSection";
 
+export const metadata: Metadata = {
+  title: "Home - Ambar",
+  description: "Essa é a página inicial da AmbarCode",
+  openGraph: {
+    title: "Home - Ambar",
+    description: "Essa é a página inicial da AmbarCode",
+    url: "https://www.seusite.com.br",
+    siteName: "AmbarAcademy",
+    locale: "pt-BR",
+    type: "website",
+    images: [
+      {
+        url: "https://postimg.cc/pyzYby4Z",
+        width: 1200,
+        height: 630,
+        alt: "Imagem de exemplo",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Home - Ambar",
+    description: "Essa é a página inicial da AmbarCode",
+    images: ["https://postimg.cc/pyzYby4Z"],
+  },
+};
 
 
 export default function Home() {
